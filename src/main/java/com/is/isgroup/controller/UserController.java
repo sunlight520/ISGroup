13c9465f9e66625aeca77666dfe5900ad7b20654
LiuChenyang <2274538435@qq.com>
2022-10-18 19:38:51 +0800

    、

--- src/main/java/com/is/isgroup/controller/UserController.java
+++ src/main/java/com/is/isgroup/controller/UserController.java
@@ -10,12 +10,10 @@ import com.is.isgroup.util.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -28,7 +26,7 @@ public class UserController extends BaseController{
         return new JsonResult<User>(OK,user1);
     }
     @RequestMapping("/login")
-    public JsonResult<User> login(String username, String password, HttpSession session) {
+    public JsonResult<User> login(HttpServletRequest request,String username, String password, HttpSession session) {
         // 调用业务对象的方法执行登录，并获取返回值
         String name = (String) session.getAttribute("username");
 //        if (!Objects.equals(username, name)) {
@@ -37,9 +35,9 @@ public class UserController extends BaseController{
         User data = userService.login(username, password);
         //登录成功后，将uid和username存入到HttpSession中
         session.setAttribute("id", data.getId());
-        session.setAttribute("username", data.getUsername());
-         System.out.println("Session中的id=" + getIdFromSession(session));
-         System.out.println("Session中的username=" + getUsernameFromSession(session));
+//        session.setAttribute("username", data.getUsername());
+//         System.out.println("Session中的id=" + getIdFromSession(session));
+         System.out.println("Session中的username=" + session.getAttribute("username"));
         // 将以上返回值和状态码OK封装到响应结果中并返回
         return new JsonResult<User>(OK, data);
     }
@@ -48,25 +46,25 @@ public class UserController extends BaseController{
         session.invalidate();
         return new JsonResult<>(OK);
     }
-    /**
-     * 后管登出
-     * @param request
-     * @return
-     */
-    @PostMapping("/logout")
-    public JsonResult<String> loginOut(HttpServletRequest request){
-        // 去除session
-
-        request.getSession().removeAttribute("username");
-        return new  JsonResult<>(OK,"退出成功");
+    @RequestMapping("/logout")
+    //退出就是将session中的用户id清理掉，需要操作session，参数要加上HttpServletRequest request
+    public JsonResult<String> logout(HttpServletRequest servletRequest,HttpSession session){
+        //清理session中保存的当前员工id
+        //获取全局对象
+        ServletContext context = ((HttpServletRequest) servletRequest).getServletContext();
+        List<String> userList= (List<String>) context.getAttribute("userList");
+        Map<String,HttpSession> sessionMap= (Map<String, HttpSession>) context.getAttribute("sessionMap");
+        String username = (String) servletRequest.getSession().getAttribute("username");
+        System.out.println();
+        userList.remove(username);
+        sessionMap.remove(username);
+        return new JsonResult<>(OK,"退出成功");
     }
-
     @RequestMapping("/updatePassword")
     public JsonResult<Void> updatePassword(String oldPassword,String password,HttpSession session){
             String username = getUsernameFromSession(session);
             userService.updatePasswordByUsername(username,password,oldPassword);
             return new JsonResult<>(OK);
     }
-
 }
 
